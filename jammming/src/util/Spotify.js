2be58717f0df6fbd7323198dbe048a1ac90522a5
LPC <laurencecook80@hotmail.co.uk>
2024-01-25 16:57:07 +0000

    adding console logs in

--- jammming/src/util/Spotify.js
+++ jammming/src/util/Spotify.js
@@ -47,7 +47,20 @@ getAccessToken() {
     const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
     window.location = accessUrl;
     console.log(`Checkpoint E: accessUrl`);
-    this.getAccessToken();
+
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      console.log(`Checkpoint D: ${accessToken}`);
+      const expiresIn = Number(expiresInMatch[1]);
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/'); // This clears the parameters, allowing us to grab a new access token when it expires.
+      return accessToken;
+    } else {
+      console.log('This hasn\'t worked');
+    }
   }
 },
 
